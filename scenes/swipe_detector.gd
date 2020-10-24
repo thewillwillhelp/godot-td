bb6de5a2b2b61b03f2db10c20d2aa36e4740d394
dee <deeppeedead@gmail.com>
2020-10-25 01:08:49 +0300

    fix: fix issue with building on touch

--- scenes/swipe_detector.gd
+++ scenes/swipe_detector.gd
@@ -4,37 +4,30 @@ signal swipe_is_happened
 signal screen_click(event)
 
 var swipe_is_started: bool = false
-var _swipe_is_stopped: bool = true
+var _screen_pressed: bool = false
 var start_position: Vector2
-var end_position: Vector2
+var move_position: Vector2
 
 func _input(event):
     if event is InputEventMouseButton:
         if event.button_index == BUTTON_LEFT:
             if event.is_pressed():
-                self._swipe_is_stopped = false
+                self._screen_pressed = true
+                self.swipe_is_started = false
                 self.start_position = event.position
+                self.move_position = event.position
             else:
-                self._swipe_is_stopped = true
+                self._screen_pressed = false
                 emit_signal("screen_click", event)
-                $Timer.start()
-                # self.end_position = event.position
-                # self.check_is_swipe_happened()
 
     if event is InputEventMouseMotion:
-        if not self._swipe_is_stopped:
-            if not self.swipe_is_started:
-                # self.swipe_is_started = true
-                $Timer.start()
-            else:
-                emit_signal("swipe_is_happened", event.position - self.start_position)
-                self.start_position = event.position
+        if not self._screen_pressed:
+            return
 
-# tricky logic
-# do not allow building right after releasing input
+        if Vector2().distance_squared_to(event.position - self.start_position) > 0:
+            emit_signal("swipe_is_happened", event.position - self.move_position)
+            self.move_position = event.position
+            self.swipe_is_started = true
 
-func _on_Timer_timeout():
-    if self._swipe_is_stopped:
-        self.swipe_is_started = false
-    else:
-        self.swipe_is_started = true
+func is_screen_moved() -> bool:
+    return self.swipe_is_started
